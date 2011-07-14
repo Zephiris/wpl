0e489ba7379eecfb32bba3a5755d48764090c447
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@b6f0c48c-fbf5-197f-bc0a-48850ea13b7e>
2011-07-14 21:52:13 +0000

    - tls primitive added to thread header/impl.

--- trunk/mt/src/thread.cpp
+++ trunk/mt/src/thread.cpp
@@ -66,5 +66,19 @@ namespace wpl
 			initialized.wait();
 			return t;
 		}
+
+
+		tls_base::tls_base()
+			: _index(::TlsAlloc())
+		{	}
+
+		tls_base::~tls_base()
+		{	::TlsFree(_index);	}
+
+		void *tls_base::get() const
+		{	return ::TlsGetValue(_index);	}
+
+		void tls_base::set(const void *value)
+		{	::TlsSetValue(_index, const_cast<void *>(value)); }
 	}
 }
