4d0a31d4740913cf3dc9eaf15b2fa56372c819aa
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@b6f0c48c-fbf5-197f-bc0a-48850ea13b7e>
2011-07-25 08:38:01 +0000

    - listview header added to wpl::ui; - ability to check emptiness of tr1::function is now used in window wrapper.

--- trunk/ui/src/window.cpp
+++ trunk/ui/src/window.cpp
@@ -1,3 +1,23 @@
+//	Copyright (C) 2011 by Artem A. Gevorkyan (gevorkyan.org)
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
 #include <win32/window.h>
 
 #include <tchar.h>
@@ -55,7 +75,7 @@ namespace wpl
 			if (message == WM_NCDESTROY)
 				::RemoveProp(hwnd, c_wrapper_ptr_name);
 
-			LRESULT result = w->_user_handler ? (*w->_user_handler)(message, wparam, lparam,
+			LRESULT result = w->_user_handler ? w->_user_handler(message, wparam, lparam,
 				wndproc(hwnd, w->_original_handler)) : w->_original_handler(hwnd, message, wparam, lparam);
 
 			if (message == WM_NCDESTROY)
@@ -99,12 +119,12 @@ namespace wpl
 
 		shared_ptr<destructible> window_wrapper::advise(const user_handler_t &user_handler)
 		{
-			_user_handler.reset(new user_handler_t(user_handler));
+			_user_handler = user_handler;
 			return shared_ptr<destructible>(new disconnector(shared_from_this()));
 		}
 
 		void window_wrapper::unadvise() throw()
-		{	_user_handler.reset();	}
+		{	_user_handler = user_handler_t();	}
 
 		HWND window_wrapper::hwnd() const
 		{	return _window;	}
