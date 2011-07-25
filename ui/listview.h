0c5d40051f6f086d2d000605fce2d524c636717d
artem.gevorkyan <artem.gevorkyan@gmail.com>
2011-07-25 08:38:01 +0000

    - listview header added to wpl::ui; - ability to check emptiness of tr1::function is now used in window wrapper.

--- ui/listview.h
+++ ui/listview.h
@@ -20,48 +20,28 @@
 
 #pragma once
 
-#include "../concepts.h"
+#include "concepts.h"
 
-#include <windows.h>
-#include <memory>
-#include <functional>
-
-namespace std
-{
-	using tr1::enable_shared_from_this;
-	using tr1::function;
-	using tr1::shared_ptr;
-}
+#include <string>
 
 namespace wpl
 {
 	namespace ui
 	{
-		class window_wrapper : public std::enable_shared_from_this<window_wrapper>
+		class listview
 		{
 		public:
-			typedef std::function<LRESULT (UINT, WPARAM, LPARAM)> original_handler_t;
-			typedef std::function<LRESULT (UINT, WPARAM, LPARAM, const original_handler_t &)> user_handler_t;
+			typedef unsigned int index_type;
+			struct datasource;
 
 		public:
-			static std::shared_ptr<window_wrapper> attach(HWND hwnd);
-			bool detach() throw();
+		};
 
-			std::shared_ptr<destructible> advise(const user_handler_t &user_handler);
-			void unadvise() throw();
+		struct listview::datasource : destructible
+		{
+			typedef listview::index_type index_type;
 
-			HWND hwnd() const;
-
-		private:
-			HWND _window;
-			WNDPROC _original_handler;
-			user_handler_t _user_handler;
-			std::shared_ptr<window_wrapper> _this;
-
-			window_wrapper(HWND hwnd);
-
-			static LRESULT CALLBACK windowproc_proxy(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-			static std::shared_ptr<window_wrapper> extract(HWND hwnd);
+			virtual void get_text(index_type item, index_type subitem, std::wstring &text) const = 0;
 		};
 	}
 }
