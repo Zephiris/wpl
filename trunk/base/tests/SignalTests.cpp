2f7375fa6a8e1d0cf5ecb8b59b38feeff0fc9649
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@b6f0c48c-fbf5-197f-bc0a-48850ea13b7e>
2011-07-28 13:56:53 +0000

    - base library (headers only) added.

---
+++ trunk/base/tests/SignalTests.cpp
@@ -0,0 +1,20 @@
+#include <signals.h>
+
+using namespace System;
+using namespace	Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace wpl
+{
+	namespace tests
+	{
+		[TestClass]
+		public ref class SignalTests
+		{
+		public: 
+			[TestMethod]
+			void TestMethod1()
+			{
+			}
+		};
+	}
+}
