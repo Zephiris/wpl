8bef72b8916988e309b19827db993508ed3b022c
artem.gevorkyan <artem.gevorkyan@gmail.com>
2011-08-16 15:01:07 +0000

    - basic functionality (display of item text) is implemented in listview; - listview ordering implementation has been started.

--- ui/src/listview.cpp
+++ ui/src/listview.cpp
@@ -22,6 +22,7 @@
 #include "../win32/window.h"
 
 #include <commctrl.h>
+#include <atlstr.h>
 
 using namespace std;
 using namespace std::tr1;
@@ -35,13 +36,17 @@ namespace wpl
 		{
 			class listview_impl : public listview
 			{
+				shared_ptr<model> _model;
 				shared_ptr<window> _listview;
-				shared_ptr<destructible> _invalidated_connection;
+				vector<sort_direction> _default_sorts;
+				shared_ptr<destructible> _advisory, _invalidated_connection;
 
-				virtual void set_model(shared_ptr<model> ds);
+				virtual void set_model(shared_ptr<model> model);
 				virtual void add_column(const wstring &caption, sort_direction default_sort_direction);
 
 				void on_invalidated(index_type new_count);
+
+				LRESULT wndproc(UINT message, WPARAM wparam, LPARAM lparam, const window::original_handler_t &previous);
 
 			public:
 				listview_impl(HWND hwnd);
@@ -50,20 +55,60 @@ namespace wpl
 
 			listview_impl::listview_impl(HWND hwnd)
 				: _listview(window::attach(hwnd))
-			{	}
-
-			void listview_impl::set_model(shared_ptr<model> ds)
 			{
-				ListView_SetItemCountEx(_listview->hwnd(), ds->get_count(), 0);
-				_invalidated_connection = ds->invalidated += bind(&listview_impl::on_invalidated, this, _1);
+				_advisory = _listview->advise(bind(&listview_impl::wndproc, this, _1, _2, _3, _4));
+			}
+
+			void listview_impl::set_model(shared_ptr<model> model)
+			{
+				ListView_SetItemCountEx(_listview->hwnd(), model->get_count(), 0);
+				_invalidated_connection = model->invalidated += bind(&listview_impl::on_invalidated, this, _1);
+				_model = model;
 			}
 
 			void listview_impl::add_column(const wstring &caption, sort_direction default_sort_direction)
-			{	throw 0;	}
+			{
+				_default_sorts.push_back(default_sort_direction);
+			}
 
 			void listview_impl::on_invalidated(index_type new_count)
 			{
 				ListView_SetItemCountEx(_listview->hwnd(), new_count, 0);
+			}
+
+			LRESULT listview_impl::wndproc(UINT message, WPARAM wparam, LPARAM lparam, const window::original_handler_t &previous)
+			{
+				if (OCM_NOTIFY == message)
+				{
+					UINT code = reinterpret_cast<const NMHDR *>(lparam)->code;
+
+					if (LVN_GETDISPINFO == code)
+					{
+						const NMLVDISPINFO *pdi = reinterpret_cast<const NMLVDISPINFO *>(lparam);
+
+						if (pdi->item.mask & LVIF_TEXT)
+						{
+							wstring b1;
+	
+							_model->get_text(pdi->item.iItem, pdi->item.iSubItem, b1);
+							CString b2(b1.c_str());
+							_tcsncpy_s(pdi->item.pszText, pdi->item.cchTextMax, b2, _TRUNCATE);
+						}
+					}
+					else if (LVN_COLUMNCLICK == code)
+					{
+						int column = reinterpret_cast<const NMLISTVIEW *>(lparam)->iSubItem;
+						sort_direction default_sort = _default_sorts[column];
+
+						if (default_sort != dir_none)
+						{
+							_model->set_order(column, default_sort == dir_ascending);
+						}
+					}
+					return 0;
+				}
+				else
+					return previous(message, wparam, lparam);
 			}
 		}
 
