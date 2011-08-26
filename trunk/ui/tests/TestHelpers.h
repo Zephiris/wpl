72c0c42caef23bf21f531fb9c346e40fbbabc2e0
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@b6f0c48c-fbf5-197f-bc0a-48850ea13b7e>
2011-08-26 07:41:21 +0000

    - reflection can now be enable on listview's host window.

--- trunk/ui/tests/TestHelpers.h
+++ trunk/ui/tests/TestHelpers.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <wpl/base/concepts.h>
+
 #include <string>
 #include <vector>
+#include <memory>
 #include <tchar.h>
+
+namespace std
+{
+	using tr1::shared_ptr;
+}
 
 namespace ut
 {
@@ -13,12 +21,14 @@ namespace ut
 	public ref class WindowTestsBase
 	{
 		std::vector<void *> *_windows;
+		std::vector< std::shared_ptr<wpl::destructible> > *_connections;
 
 	protected:
 		void *create_window();
 		void *create_visible_window();
 		void *create_window(const TCHAR *class_name);
 		void *create_window(const TCHAR *class_name, void *parent, unsigned int style, unsigned int stylex);
+		void enable_reflection(void *hwnd);
 
 		void destroy_window(void *hwnd);
 
