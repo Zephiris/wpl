c093971043d3f9ff174f2e6134bd058d1605dcd1
artem.gevorkyan <artem.gevorkyan@b6f0c48c-fbf5-197f-bc0a-48850ea13b7e>
2014-09-30 13:12:57 +0000

    - some tests and compilation fixed for VS 9.0.

--- ui/samples/main.cpp
+++ ui/samples/main.cpp
@@ -5,6 +5,11 @@
 #include <wpl/ui/layout.h>
 #include <wpl/ui/listview.h>
 
+namespace std
+{
+	using tr1::static_pointer_cast;
+}
+
 using namespace std;
 using namespace wpl;
 using namespace wpl::ui;
